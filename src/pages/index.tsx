d4970700931671616b24cddd922100f41cde0c92
Minho Kang <minhokang@minhos-mbp-2.home>
2020-07-21 19:00:04 -0400

    trying out lazyload

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,51 @@
-import React from "react"
+import React, { ComponentType } from "react"
 import { Link } from "gatsby"
 
 import Image from "../components/image"
 import SEO from "../components/seo"
 import { useTranslation } from "react-i18next"
+import { Router } from '@reach/router';
+
+const Contact = React.lazy(() => import('../components/Contact'));
+const About = React.lazy(() => import('../components/About'));
+
+const LazyComponent: ComponentType<{
+  Component: ComponentType<any>;
+  [key: string]: any;
+}> = 
+  (props: any) => {
+    const Component = props.Component;
+    return (
+      <React.Suspense fallback={'<p>Loading...</p>'}>
+        <Component {...props} />
+      </React.Suspense>
+    )
+  };
+
+const Home: ComponentType<any> = () => <h2>Hello and Welcome</h2>;
+
 
 const IndexPage = () => {
   const { t } = useTranslation();
   return (
     <>
       <SEO title="Home" />
+
       <h1>{t('home.title')}</h1>
       <p>{t('home.subline')}</p>
-      <p>Now go build something great.</p>
-      <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-        <Image />
-      </div>
-      <Link to="/page-2/">{t('home.backToHome')}</Link>
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+
+      <Link to="/">Home</Link>
+      <br />
+      <Link to="/contact/">Contact</Link>
+      <br />
+      <Link to="/about-us">About Us</Link>
+      <br />
+
+      <Router>
+        <Home path="/" />
+        <LazyComponent Component={Contact} path="contact" />
+        <LazyComponent Component={About} path="about-us" />
+      </Router>
     </>
   )
 }
